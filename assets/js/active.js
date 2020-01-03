6917d315bec2babbcd8ff99e31a67ac3b4a5a0fc
Md. Nahid Hasan <32294128+Nahid01920@users.noreply.github.com>
2020-01-03 10:55:26 +0600

    my new web page

---
+++ assets/js/active.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+    $('.link').click(function(){
+        $('.link').toggleClass('active')
+    })
+    
+    $('.link').click(function(){
+        $('.link').toggleClass('active')
+    })
+
+})
